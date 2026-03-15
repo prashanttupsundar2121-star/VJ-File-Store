fc8ce57baf89dfdcc1dd209374945bc9171d1484
prashanttupsundar2121-star <prashanttupsundar2121@gmail.com>
2026-03-15 18:48:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -80,7 +80,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://web-production-20349.up.railway.app/")
+URL = environ.get("URL", "")
 
 
 # Don't Remove Credit Tg - @VJ_Bots
